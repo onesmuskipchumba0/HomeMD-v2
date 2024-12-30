f2dc254a71ba65ffb25a9d18bf4ec08c62e66dde
OnesmusBett <onesmuskipchumba5@gmail.com>
2024-12-30 09:21:18 +0300

    Final commit

--- app.config.ts
+++ app.config.ts
@@ -1,12 +1,50 @@
-export default {
-    expo: {
-      // ... other config
-      extra: {
-        GEMINI_API_KEY: process.env.EXPO_PUBLIC_GEMINI_API_KEY,
-      },
-      android: {
-        package: "com.onesmus.homemd",
-      }
+import 'dotenv/config'; // Import dotenv for local .env support
+
+export default () => ({
+  expo: {
+    name: "HomeMD",
+    slug: "homemd",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/logo.png",
+    scheme: "myapp",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
     },
-  };
-  
+    android: {
+      package: "com.onesmus.homemd", // Fixed package name
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/logo.png",
+        backgroundColor: "#ffffff",
+      },
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/logo.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+    },
+    extra: {
+      GEMINI_API_KEY: process.env.EXPO_PUBLIC_GEMINI_API_KEY, // Environment variable
+      eas: {
+        projectId: "9854dc48-d5e9-4ccb-b7f5-3c2e665b99e9", // EAS project ID
+      },
+    },
+  },
+});
